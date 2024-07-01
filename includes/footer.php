30d570533fdd0d1930f1ec27a032d4840a918186
Carlos Cervantes <carlosgahelpulidocervantes@gmail.com>
2024-07-01 15:01:44 -0700

    Cambiada la estructura de los archivos de la carpeta includes

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+    <footer>&copy;<span id="2024">2024</span>.<span> Trabajo para "Bases de datos distribuidas". UNIVAM.</span></footer>
+</body>
+
+</html>
--- scripts/login/login.php
+++ scripts/login/login.php
@@ -18,31 +18,44 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
 // Verificar si la solicitud es desde un entorno web
-if (!isset($_SERVER['REQUEST_METHOD'])) {
-    echo "<html><body><h1>Error: Este script debe ejecutarse en un entorno de servidor web.</h1></body></html>";
-    exit;
-}
+if (php_sapi_name() !== 'cli' && isset($_SERVER['REQUEST_METHOD'])) {
+    // Activar el output buffering
+    ob_start();
 
-// Verificar si la solicitud es POST
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    echo "<html><body><h1>Método no permitido</h1></body></html>";
-    exit;
-}
+    // Incluir archivos PHP y capturar su contenido
+    include __DIR__ . '/../../includes/head.php';
+    $html = ob_get_clean();
 
-// Obtener y verificar los datos de la solicitud
-$username = $_POST['username'] ?? null;
-$password = $_POST['password'] ?? null;
+    include __DIR__ . '/../../includes/header.php';
+    $html .= ob_get_clean();
 
-if (!isset($username) || !isset($password)) {
-    echo "<html><body><h1>Petición inválida</h1></body></html>";
-    exit;
-}
+    // Verificar si la solicitud es POST
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        $html .= "<h1>Error: Este script debe ejecutarse con el método POST.</h1>";
+    } else {
+        // Obtener y verificar los datos de la solicitud
+        $username = $_POST['username'] ?? null;
+        $password = $_POST['password'] ?? null;
 
-// Verificar credenciales
-if (checkCredentials($username, $password)) {
-    echo "<html><body><h1>Inicio de sesión exitoso</h1></body></html>";
+        if (!isset($username) || !isset($password)) {
+            $html .= "<h1>Parámetros incorrectos</h1>";
+        } else {
+            // Verificar credenciales
+            if (checkCredentials($username, $password)) {
+                $html .= "<h1>Bienvenido, $username</h1>";
+            } else {
+                $html .= "<h1>Credenciales incorrectas</h1>";
+            }
+        }
+    }
+
+    
+    include __DIR__ . '/../../includes/footer.php';
+    $html .= ob_get_clean();
+
+    echo $html;
 } else {
-    echo "<html><body><h1>Credenciales inválidas</h1></body></html>";
+    echo "<h1>Error: Este script debe ejecutarse en un entorno de servidor web.</h1>";
 }
 
 /**
